723a807dfb2d0ce6bfbe2ec6810b02014618c546
ilhamdwikarifani <ilhamdwiki0408@gmail.com>
2023-10-31 20:17:56 +0700

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      urbanist: ["Urbanist", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+
+{
+  /* <div class="latestpost-content wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s"></div> */
+}
